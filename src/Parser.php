a79a069effd12a8b13d63c829b6ca01c5ac251eb
oscarotero <oom@oscarotero.com>
2019-05-24 19:53:39 +0200

    improved encoding

--- src/Parser.php
+++ src/Parser.php
@@ -12,9 +12,26 @@ use SimpleXMLElement;
 class Parser
 {
     private const ENCODINGS = [
-        'UTF-8' => 'utf-8',
-        'SJIS' => 'shift_jis',
-        'ISO-8859-1' => 'utf-8',
+        'ASCII'        => 'ascii',
+        'UTF-8'        => 'utf-8',
+        'SJIS'         => 'shift_jis',
+        'Windows-1251' => 'windows-1251',
+        'Windows-1252' => 'windows-1252',
+        'Windows-1254' => 'windows-1254',
+        'ISO-8859-1'   => 'iso-8859-1',
+        'ISO-8859-2'   => 'iso-8859-2',
+        'ISO-8859-3'   => 'iso-8859-3',
+        'ISO-8859-4'   => 'iso-8859-4',
+        'ISO-8859-5'   => 'iso-8859-5',
+        'ISO-8859-6'   => 'iso-8859-6',
+        'ISO-8859-7'   => 'iso-8859-7',
+        'ISO-8859-8'   => 'iso-8859-8',
+        'ISO-8859-9'   => 'iso-8859-9',
+        'ISO-8859-10'  => 'iso-8859-10',
+        'ISO-8859-13'  => 'iso-8859-13',
+        'ISO-8859-14'  => 'iso-8859-14',
+        'ISO-8859-15'  => 'iso-8859-15',
+        'ISO-8859-16'  => 'iso-8859-16',
     ];
 
     public static function stringify(DOMNode $node): string
@@ -65,12 +82,19 @@ class Parser
 
     private static function normalize(string $string): string
     {
-        foreach (self::ENCODINGS as $encoding => $contentType) {
-            if (mb_detect_encoding($string, $encoding, true) === $encoding) {
-                $string = mb_convert_encoding($string, 'HTML-ENTITIES', $encoding);
+        if (stripos($string, '<meta charset=') === false) {
+            return $string;
+        }
 
-                return preg_replace('/<head[^>]*>/', "<head><META HTTP-EQUIV='Content-Type' CONTENT='text/html; charset='{$contentType}'>", $string);
-            }
+        $detected = mb_detect_encoding($string, implode(',', array_keys(self::ENCODINGS)), true);
+        
+        if ($detected && isset(self::ENCODINGS[$detected])) {
+            $string = mb_convert_encoding($string, 'HTML-ENTITIES', $detected);
+            $string = preg_replace(
+                '/<head[^>]*>/',
+                '<head><META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset='.self::ENCODINGS[$detected].'">',
+                $string
+            );
         }
 
         return $string;
