a41913e5177370e6dd3f3143fc1347814ef07b52
Oscar Otero <oscar@marketgoo.com>
2019-12-15 19:09:58 +0100

    encoding detection issues

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,8 +2,12 @@
 
 All notable changes to this project will be documented in this file.
 
-The format is based on [Keep a Changelog](http://keepachangelog.com/) 
+The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
+
+## [0.1.4] - 2019-12-15
+### Fixed
+- Changed the way to handle different encodings detecting the <meta charset> and <meta http-equiv> elements
 
 ## [0.1.3] - 2019-07-20
 ### Fixed
@@ -20,6 +24,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 ## 0.1.0 - 2019-04-21
 First version
 
+[0.1.4]: https://github.com/oscarotero/html-parser/compare/v0.1.3...v0.1.4
 [0.1.3]: https://github.com/oscarotero/html-parser/compare/v0.1.2...v0.1.3
 [0.1.2]: https://github.com/oscarotero/html-parser/compare/v0.1.1...v0.1.2
 [0.1.1]: https://github.com/oscarotero/html-parser/compare/v0.1.0...v0.1.1
--- src/Parser.php
+++ src/Parser.php
@@ -8,32 +8,10 @@ use DOMNode;
 use DOMDocument;
 use DOMDocumentFragment;
 use SimpleXMLElement;
+use DOMXPath;
 
 class Parser
 {
-    private const ENCODINGS = [
-        'ASCII'        => 'ascii',
-        'UTF-8'        => 'utf-8',
-        'SJIS'         => 'shift_jis',
-        'Windows-1251' => 'windows-1251',
-        'Windows-1252' => 'windows-1252',
-        'Windows-1254' => 'windows-1254',
-        'ISO-8859-1'   => 'iso-8859-1',
-        'ISO-8859-2'   => 'iso-8859-2',
-        'ISO-8859-3'   => 'iso-8859-3',
-        'ISO-8859-4'   => 'iso-8859-4',
-        'ISO-8859-5'   => 'iso-8859-5',
-        'ISO-8859-6'   => 'iso-8859-6',
-        'ISO-8859-7'   => 'iso-8859-7',
-        'ISO-8859-8'   => 'iso-8859-8',
-        'ISO-8859-9'   => 'iso-8859-9',
-        'ISO-8859-10'  => 'iso-8859-10',
-        'ISO-8859-13'  => 'iso-8859-13',
-        'ISO-8859-14'  => 'iso-8859-14',
-        'ISO-8859-15'  => 'iso-8859-15',
-        'ISO-8859-16'  => 'iso-8859-16',
-    ];
-
     public static function stringify(DOMNode $node): string
     {
         if ($node instanceof DOMDocument) {
@@ -45,16 +23,30 @@ class Parser
 
     public static function parse(string $html): DOMDocument
     {
-        $errors = libxml_use_internal_errors(true);
-        $entities = libxml_disable_entity_loader(true);
+        $detected = mb_detect_encoding($html);
+        
+        if ($detected) {
+            $html = mb_convert_encoding($html, 'HTML-ENTITIES', $detected);
+        }
 
-        $html = trim(self::normalize($html));
+        $document = self::createDOMDocument($html);
+        $xpath = new DOMXPath($document);
 
-        $document = new DOMDocument();
-        $document->loadHTML($html);
+        $charset = $xpath->query('.//meta[@charset]')->item(0);
+        $httpEquiv = $xpath->query('.//meta[@http-equiv]')->item(0);
 
-        libxml_use_internal_errors($errors);
-        libxml_disable_entity_loader($entities);
+        if ($charset || $httpEquiv) {
+            $charset = $charset ? self::stringify($charset) : null;
+            $httpEquiv = $httpEquiv ? self::stringify($httpEquiv) : null;
+
+            $html = preg_replace(
+                '/<head[^>]*>/',
+                '<head>'.$charset.$httpEquiv,
+                $html
+            );
+
+            return self::createDOMDocument($html);
+        }
 
         return $document;
     }
@@ -80,19 +72,17 @@ class Parser
         return $fragment;
     }
 
-    private static function normalize(string $string): string
+    private static function createDOMDocument(string $code): DOMDocument
     {
-        $detected = mb_detect_encoding($string, implode(',', array_keys(self::ENCODINGS)), true);
-        
-        if ($detected && isset(self::ENCODINGS[$detected])) {
-            $string = mb_convert_encoding($string, 'HTML-ENTITIES', $detected);
-            $string = preg_replace(
-                '/<head[^>]*>/',
-                '<head><META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset='.self::ENCODINGS[$detected].'">',
-                $string
-            );
-        }
+        $errors = libxml_use_internal_errors(true);
+        $entities = libxml_disable_entity_loader(true);
 
-        return $string;
+        $document = new DOMDocument();
+        $document->loadHTML($code);
+
+        libxml_use_internal_errors($errors);
+        libxml_disable_entity_loader($entities);
+
+        return $document;
     }
 }
